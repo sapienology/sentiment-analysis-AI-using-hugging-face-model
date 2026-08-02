ef65b16b86a7d9bc3c66661379311e1973ba8dd9
sapienology <ndubestanthony@gmail.com>
2026-08-02 22:27:28 +0100

    Merge remote changes

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,16 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-MODEL_NAME = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+MODEL_NAME = "distilbert-base-uncased-finetuned-sst-2-english"
 
 
-tokenizer = AutoTokenizer.from_pretrained(
-    MODEL_NAME
-)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
 
-model = AutoModelForSequenceClassification.from_pretrained(
-    MODEL_NAME
-)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
 
 
 labels = model.config.id2label
